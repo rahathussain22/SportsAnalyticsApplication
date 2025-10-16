49cc607117ef978ca5b3a73e8c72cd21a7df1df2
rahathussain22 <rahathussain2323@gmail.com>
2025-10-16 16:23:52 +0500

    SportAnalyticsApplication backend setup complete

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import http from "http"
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import { Server } from "socket.io";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+// creating io connection server
+const server= http.createServer(app)
+
+// Middlewares
+app.use(cors({
+  origin: true, // Reflect the request origin, as defined by the request's Origin header
+  credentials: true
+}));
+
+app.use(express.json());
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Route to serve public.html directly
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "public.html"));
+});
+
+import { userRoute } from "./src/routes/user.route.js";
+app.use('/user',userRoute)
+const io =new Server(server)
+io.on('connection', (socket) =>{
+console.log("a new user has connected", socket.id)
+})
+
+export default app;
