41e4d4dd0402ccf60bd45449158a0d3c983dde94
Muhammad Haris <yassuopro316@gmail.com>
2025-10-20 12:11:35 +0500

    Delete backend directory

--- backend/src/routes/league.route.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { addLeague } from "../controllers/league.controller.js";
-const leagueRouter = Router()
-leagueRouter.route('/addLeague').post(addLeague)
-export {leagueRouter}
--- backend/src/routes/team.route.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import { addTeam } from "../controllers/team.controller.js";
-const teamRouter = Router()
-teamRouter.route('/addTeam').post(addTeam)
-export {teamRouter}
